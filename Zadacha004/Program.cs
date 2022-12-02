4fb3876df530a7268b252eb5edb6982d03f23e4d
Aleksandr Onufriev <pskmp@yandex.ru>
2022-12-02 18:47:14 +0300

    dobavil Zadacha004

---
+++ Zadacha004/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 4. Написать программу возведения числа А в целую стень B
+
+int PowNum(int A, int B)
+{
+    if (B == 0) return 1;
+    else if (B == 1) return A;
+    return A * PowNum(A, B - 1);
+}
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите степень B: ");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine($"Число {A} возведенное в степень {B} равно {PowNum(A, B)}");
